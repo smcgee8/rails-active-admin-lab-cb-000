396cf166a50786fc31a5603ff97c8b7e0e8a791d
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-08 23:15:51 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,42 +5,42 @@ class ArtistsController < ApplicationController
   def show
   end
 
-  def new
-    @artist = Artist.new
-  end
+  # def new
+  #   @artist = Artist.new
+  # end
+  # 
+  # def create
+  #   @artist = Artist.new(artist_params)
+  # 
+  #   if @artist.save
+  #     redirect_to @artist
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def create
-    @artist = Artist.new(artist_params)
+  # def edit
+  #   @artist = Artist.find(params[:id])
+  # end
+  # 
+  # def update
+  #   @artist = Artist.find(params[:id])
+  # 
+  #   @artist.update(artist_params)
+  # 
+  #   if @artist.save
+  #     redirect_to @artist
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-    if @artist.save
-      redirect_to @artist
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @artist = Artist.find(params[:id])
-  end
-
-  def update
-    @artist = Artist.find(params[:id])
-
-    @artist.update(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-    flash[:notice] = "Artist deleted."
-    redirect_to artists_path
-  end
+  # def destroy
+  #   @artist = Artist.find(params[:id])
+  #   @artist.destroy
+  #   flash[:notice] = "Artist deleted."
+  #   redirect_to artists_path
+  # end
 
   private
 
