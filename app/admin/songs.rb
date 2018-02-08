e945949256680dce6ba16455ac98e8791a1e6e01
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-08 23:19:18 +0000

    automatically backed up by learn

--- app/admin/songs.rb
+++ app/admin/songs.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Song do
+  actions :all, except: [:destroy]
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
