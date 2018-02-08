83b80c1593af8910e5cc39820b3f9f0bebc4a810
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-08 23:14:19 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  resources :artists, :songs, only: [:show, :index]
 
   root to: 'artists#index'
 end
